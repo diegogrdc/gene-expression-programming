a9b40a8b479414ba65ef751684d71659fe055514
Diego García <diegomysterio@gmail.com>
2022-04-07 22:08:06 -0500

    added comments to code

--- implementation/MCS-MO/Population.py
+++ implementation/MCS-MO/Population.py
@@ -10,23 +10,34 @@ class Population:
     # Initialize a population with all its relevant information
     # generations_cnt = Number of generations that population will evolve
     # population_sz = Number of individuals on each generation
-    def __init__(self, generations_cnt, population_sz, gene_cnt, class_cnt, head_sz, function_set, terminal_set):
+    # gene_cnt = Number of ADF genes of each individual
+    # class cnt = Number of Homeotic genes of each individual
+    # head_sz = Head size of each gene
+    # Function_set = functions used on gene expression
+    # terminal_set = terminals used on gene expression
+    def __init__(self, generations_cnt, population_sz, gene_cnt, class_cnt, head_sz, function_set, terminal_set_sz):
         # We store number of generations that we will evolve our chromosomes
         self.generations = generations_cnt
         # We store population size
         self.pop_sz = population_sz
+        # We store head size as information
         self.h = head_sz
+        # We store class count as information
         self.c = class_cnt
+        # We store gene count as information
         self.g = gene_cnt
+        # We store function set as information for expression
         self.fn_set = function_set
-        self.tm_set = terminal_set
+        # We store terminal set size. When checking fitness, they will be used in order
+        # as determined by the fitness evaluator
+        self.tm_set_sz = terminal_set_sz
         self.t = self.getTailSize()
 
         # We generate initial population
         self.population = []
         for _ in range(0, self.pop_sz):
             self.population.append(Chromosome(
-                self.h, self.t, self.g, self.c, len(self.fn_set), len(self.tm_set)))
+                self.h, self.t, self.g, self.c, len(self.fn_set), self.tm_set_sz))
 
     def getTailSize(self):
         return max(list(map(lambda fn: fn.__code__.co_argcount, self.fn_set)))
--- implementation/MCS-MO/TestPopulation.py
+++ implementation/MCS-MO/TestPopulation.py
@@ -22,7 +22,7 @@ def div(x, y):
 def test():
     function_set = [add, sub, mul, div]
     terminal_set = ['a', 'b', 'c', 'd', 'e']
-    pop = Population(10, 10, 3, 3, 5, function_set, terminal_set)
+    pop = Population(10, 10, 3, 3, 5, function_set, len(terminal_set))
     for i, chrom in enumerate(pop.population):
         print("Individual #", i)
         chrom.printChromosome()
