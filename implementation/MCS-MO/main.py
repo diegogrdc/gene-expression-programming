5c1f769ca6e1bc53c347ae0e24b65c43f227bd7b
Diego García <diegomysterio@gmail.com>
2022-04-07 22:16:42 -0500

    clean main

--- implementation/MCS-MO/main.py
+++ implementation/MCS-MO/main.py
@@ -1,17 +1,5 @@
-from Chromosome import Chromosome
-
-
-def add(x, y):
-    return x + y
-
-
-def sub(x, y):
-    return x - y
-
-
 def main():
-    print("Multi Cellular System - Multiple Outputs")
-    x = Chromosome(2, [add, sub], [])
+    pass
 
 
 main()
