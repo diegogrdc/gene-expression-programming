c404a8df885846d0fd8987dc2d5e59eb97ffc4df
Diego García <diegomysterio@gmail.com>
2022-04-07 23:12:34 -0500

    add fitness roulette

--- implementation/MCS-MO/Population.py
+++ implementation/MCS-MO/Population.py
@@ -1,5 +1,7 @@
 from Chromosome import Chromosome
 from FitnessEvaluator import FitnessEvaluator
+import numpy as np
+import copy
 
 # Population class representas all chromosomes on each generation.
 # It starts with a random generation of chromosomes, and it evaluates their fitness
@@ -54,30 +56,66 @@ class Population:
     def run(self):
         # We iterate for each generation
         for _ in range(0, self.generations):
-            # We evaluate the fitness of each individual
-            # It is stored in self.fitness list
-            self.evalFitness()
+            self.runGeneration()
 
-        # We look for best of generation
-        best_fitness = max(self.fitness)
-        best_idx = self.fitness.index(best_fitness)
+    # This function contains the run in one generation
+    # From evaulation, to selection and modification
+    def runGeneration(self):
+        # We evaluate the fitness of each individual
+        # It is stored in self.fitness list
+        # It also creates the fitness roulettes
+        self.evalFitness()
+
         # We create a new empty generation
         new_population = []
         # We keep the best due to elitism
-        new_population.append(self.population[best_idx])
-        # We need to do pop_sz - 1 new chromosomes
+        new_population.append(self.getBestOfGeneration())
+
+        # We need to create another (pop_sz - 1) new chromosomes
         # Remember we choose the parent gene with roulette wheel based on fitness
-        # TODO: Implement roulette wheel and use modification class
-        # to modify gene as needed
         for _ in range(0, self.pop_sz - 1):
-            pass
+            # We get a parent to replicate from the roulette
+            ind = self.getIndividualFromRoulette()
+            # TODO: IMPLEMENT MODIFICATIONS
+
+    # This function gets the best individual in a generation
+    # based on the calculated fitness
+
+    def getBestOfGeneration(self):
+        best_fitness = max(self.fitness)
+        best_idx = self.fitness.index(best_fitness)
+        return self.population[best_idx]
 
     # We evaluate fitness for each individual, and keep it stored in
     # self.fitness[] list, so we can use the fitness on the evolutionary process
-
+    # Each time we calculate fitness, we need to obtain a new fitness
+    # roulette to choose modifications, so it also calculates and creates this
+    # and stores it in self.roulette
     def evalFitness(self):
         self.fitness = self.fitness_eval.evaluatePopulationFitness(
             self.population)
+        self.roulette = self.getFitnessRoulette()
+
+    # We generate the fitness roulette
+    # Remember after calculating fitness
+    # we give bigger slices to bigger fitness
+    # so they have a bigger chance on reproduction
+    def getFitnessRoulette(self):
+        # First we need sum of total fitness
+        total = sum(self.fitness)
+        # Then, we assign roulette vals as needed
+        self.roulette = []
+        act = 0
+        for i in range(0, self.population):
+            act = act + self.fitness[i]
+            self.roulette.append(act / total)
+
+    def getIndividualFromRoulette(self):
+        x = np.random.random()
+        for i in range(0, self.population):
+            if x < self.roulette[i]:
+                return copy.deepcopy(self.population[i])
+        return copy.deepcopy(self.population[-1])
 
     # We calculate the tail size that we need with the formula
     # t = h * (n_max - 1) + 1
